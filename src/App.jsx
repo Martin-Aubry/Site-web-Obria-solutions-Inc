f885f339ddf96b2cc1ec564e74cb71f918ea932a
Martin Aubry <martinaubry1983@gmail.com>
2025-05-10 11:40:31 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <img src="/logo.png" alt="Logo Tech Optimisation" className="logo" />
+        <h1>Le futur de l'approvisionnement public</h1>
+        <p>
+          Notre plateforme est en cours d'élaboration.
+          <br />
+          Restez à l’affût !
+        </p>
+        <a href="mailto:info@obria.ca" className="contact-button">
+          Nous écrire
+        </a>
+      </div>
+      <footer>&copy; 2025 Obria solutions Inc.</footer>
+    </>
+  );
+}
+
+export default App;
